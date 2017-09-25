9c2e79c50a6c66ec9438babfe6b6c01e80ba3fce
Matt.Lyons <matt.lyons@envisionthen.com>
2017-09-25 08:09:35 -0400

    Bitwise changes
    Reduced overhead by eliminating Array.Copy and managing all state via 3 long variables (y axis line, up diag, down diag)

--- QueenSolutionConsole.cs
+++ QueenSolutionConsole.cs
@@ -9,7 +9,7 @@ namespace QueenSolutionConsole
     {
 
         //constant table size >=4
-        private const int _BoardSize = 12;
+        private const int _BoardSize = 15;
         private static long _Count;
         private static long _Spin;
         private static int[][] _Loops = new int[_BoardSize][];
@@ -21,84 +21,77 @@ namespace QueenSolutionConsole
         public static void Main(string[] args)
         {
             var sw = System.Diagnostics.Stopwatch.StartNew();
-
-            BuildBlankSet();
-            BuildStateLoops();
-
+  
             _Count = 0;
             _Spin = 0;
 
-            BuildNew(0, (_BoardSize + 1) / 2);
+            BuildNew(0, (_BoardSize + 1) / 2, 0, 0, 0);
+            
             sw.Stop();
             Console.WriteLine($"Board Size = {_BoardSize}  Total Spin={_Spin}");
             Console.WriteLine($"Time {sw.ElapsedMilliseconds} milliseconds  Solutions={_Solutions.Count}");
-           
+
         }
 
-        //initialize static current solution array
-        private static void BuildBlankSet()
-        {
-            for (int j = 0; j < _BoardSize; j++)
-                _Current[j] = -1;
-        }
-        //initialize static stateful loop arrays
-        private static void BuildStateLoops()
-        {
-            for (int x = 0; x < _BoardSize; x++)
-            {
-                _Loops[x] = new int[_BoardSize];
-                for (int y = 0; y < _BoardSize; y++)
-                    _Loops[x][y] = y;
-            }
-        }
-
+      
+   
         //recursive method to iterate all potential positions
-        //only include valid solutions
         //maintain state as we go so when we come back to a previous loop and iterate, we don't have to rebuild the entire state
         //only iterate through half as we use Y-flip to find other solutions
-        private static void BuildNew(int position, int end)
+        private static void BuildNew(int position, int end, long yline, long updiag, long downdiag)
         {
-            int[][] state = new int[_BoardSize][];
-            for (int x = 0; x < _BoardSize; x++)
-                state[x] = new int[_BoardSize];
+            long YLine = 0;
+            long UpDiag = 0;
+            long DownDiag = 0;
+            bool test = true;
+
+            long tempY = 0;
+            long tempUp = 0;
+            long tempDown = 0;
 
             for (int x = 0; x < end; x++)
             {
-                if (_Loops[position][x] >= 0)
-                {
-                    _Spin++;
-                    _Current[position] = x;
+                _Spin++;
+                _Current[position] = x;
 
+                tempY = 2 << x;
+                tempUp = 2 << (_BoardSize + x - position);
+                tempDown = 2 << (position + x);
+
+                test = (tempY & yline) != tempY;
+                test = test && ((tempDown & downdiag) != tempDown);
+                test = test && ((tempUp & updiag) != tempUp);
+
+                if (test)
+                {
                     if (position == _BoardSize - 1)
                     {
                         _Count++;
                         _Solutions.TryAdd(string.Join(",", _Current), _Current);
-
                         _Rotated = YFlip(_Current);
                         _Solutions.TryAdd(string.Join(",", _Rotated), _Rotated);
                     }
                     if (position < _BoardSize - 1)
                     {
-                        for (int i = 1; position + i < _BoardSize; i++)
-                            Array.Copy(_Loops[position + i], state[position + i], _BoardSize);
+                        YLine = yline;
+                        UpDiag = updiag;
+                        DownDiag = downdiag;
 
-                        for (int i = 1; position + i < _BoardSize; i++)
-                        {
-                            _Loops[position + i][x] = -1;
-                            if (x - i >= 0)
-                                _Loops[position + i][x - i] = -1;
-                            if (x + i < _BoardSize)
-                                _Loops[position + i][x + i] = -1;
-                        }
-                        BuildNew(position + 1, _BoardSize);
+                        yline = tempY | yline;
+                        downdiag = tempDown | downdiag;
+                        updiag = tempUp | updiag;
 
+                        BuildNew(position + 1, _BoardSize, yline, updiag, downdiag);
 
-                        for (int i = 1; position + i < _BoardSize; i++)
-                            Array.Copy(state[position + i], _Loops[position + i], _BoardSize);
+                        yline = YLine;
+                        updiag = UpDiag;
+                        downdiag = DownDiag;
                     }
                 }
+                test = true;
             }
         }
+
 
         //not used yet
         private static int[] XFlip(int[] placed)
@@ -121,6 +114,47 @@ namespace QueenSolutionConsole
             int[] test = new int[_BoardSize];
             for (int x = 0; x < _BoardSize; x++)
                 test[x] = _BoardSize - 1 - placed[x];
+
+            return test;
+        }
+        private static int[] Rotate90(int[] placed)
+        {
+            int[] test = new int[_BoardSize];
+            //id mid point
+            var mid = ((double)_BoardSize - 1.0) / 2.0;
+            double xStart;
+            double yStart;
+            double xEnd;
+            double yEnd;
+
+            for (int x = 0; x < _BoardSize; x++)
+            {
+                if (placed[x] >= 0)
+                {
+                    if (x < mid)
+                        xStart = Math.Floor((double)x - mid);
+                    else
+                        xStart = Math.Ceiling((double)x - mid);
+                    if (placed[x] < mid)
+                        yStart = Math.Floor((double)placed[x] - mid);
+                    else
+                        yStart = Math.Ceiling((double)placed[x] - mid);
+
+                    xEnd = yStart;
+                    yEnd = xStart * -1;
+
+                    if (xEnd < 0)
+                        xEnd = Math.Ceiling(mid + xEnd);
+                    else
+                        xEnd = Math.Floor(mid + xEnd);
+                    if (yEnd < 0)
+                        yEnd = Math.Ceiling(mid + yEnd);
+                    else
+                        yEnd = Math.Floor(mid + yEnd);
+
+                    test[(int)xEnd] = (int)yEnd;
+                }
+            }
 
             return test;
         }
